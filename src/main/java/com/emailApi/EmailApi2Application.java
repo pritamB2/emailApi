be87fc3e5be956db6e576cf7f15998ef5d3d7eed
Pritam Biswas <emailtobiswas2000@gmail.com>
2023-10-17 21:37:11 +0530

    initial commit

---
+++ src/main/java/com/emailApi/EmailApi2Application.java
@@ -0,0 +1,13 @@
+package com.emailApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/emailApi/EmailApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
